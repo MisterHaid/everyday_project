b46e1e10a95d01b27ae8f16ca86cdd2e7eb1f8cd
MisterHaid <imamhaid03.gmail.com>
2024-03-19 19:39:12 +0300

    Update

---
+++ validations/auth.js
@@ -0,0 +1,8 @@
+import { body} from "express-validator";
+
+export const registerValidation=[
+    body('email',' Wrong email format').isEmail(),
+    body('password', ' The password size is minimum 5 symbols').isLength({min:5}),
+    body('fullName','Input your fullname').isLength({min:3}),
+    body('avatarUrl',' Wrong avatar link').optional().isURL(),
+];
